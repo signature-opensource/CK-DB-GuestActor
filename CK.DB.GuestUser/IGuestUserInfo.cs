bfa4b4dc3de36f0d7e22a39c1a8caf37236e5949
Hugo Zammit <hugo.zammit@signature-code.com>
2019-05-15 11:30:48 +0200

    Add missing parameters to IGuestUserInfo poco.

--- CK.DB.GuestUser/IGuestUserInfo.cs
+++ CK.DB.GuestUser/IGuestUserInfo.cs
@@ -1,3 +1,4 @@
+using System;
 using CK.Core;
 
 namespace CK.DB.GuestUser
@@ -5,5 +6,9 @@ namespace CK.DB.GuestUser
     public interface IGuestUserInfo : IPoco
     {
         string Token { get; set; }
+
+        DateTime? ExpirationDateUtc { get; set; }
+
+        bool? Active { get; set; }
     }
 }

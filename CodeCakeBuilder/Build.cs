98c26ee73e7a1e967c820c5ff1d36fc3e628043f
Olivier Spinelli <olivier.spinelli@invenietis.com>
2019-05-17 08:45:06 +0200

    Initialized build script.

---
+++ CodeCakeBuilder/Build.cs
@@ -0,0 +1,117 @@
+using Cake.Common;
+using Cake.Common.Solution;
+using Cake.Common.IO;
+using Cake.Common.Tools.MSBuild;
+using Cake.Common.Tools.NuGet;
+using Cake.Core;
+using Cake.Common.Diagnostics;
+using Code.Cake;
+using Cake.Common.Tools.NuGet.Pack;
+using System.Linq;
+using Cake.Core.Diagnostics;
+using Cake.Common.Tools.NuGet.Restore;
+using System;
+using Cake.Common.Tools.NuGet.Push;
+using SimpleGitVersion;
+using Cake.Common.Tools.NUnit;
+using System.Collections.Generic;
+using Cake.Common.Text;
+using Cake.Core.IO;
+using System.Diagnostics;
+using System.Xml.Linq;
+using System.Reflection;
+using Cake.Common.Tools.DotNetCore;
+using Cake.Common.Tools.DotNetCore.Restore;
+using Cake.Common.Tools.DotNetCore.Build;
+using Cake.Common.Tools.DotNetCore.Pack;
+using Cake.Common.Build;
+using System.Data.SqlClient;
+using Cake.Common.Tools.NuGet.Install;
+using System.Net.Http;
+using Cake.Common.Net;
+using Cake.Common.Tools.DotNetCore.Publish;
+
+namespace CodeCake
+{
+
+    /// <summary>
+    /// Sample build "script".
+    /// Build scripts can be decorated with AddPath attributes that inject existing paths into the PATH environment variable. 
+    /// </summary>
+    [AddPath( "%UserProfile%/.nuget/packages/**/tools*" )]
+    public partial class Build : CodeCakeHost
+    {
+        public Build()
+        {
+            Cake.Log.Verbosity = Verbosity.Diagnostic;
+
+            var solutionFileName = Cake.Environment.WorkingDirectory.GetDirectoryName() + ".sln";
+
+            var projects = Cake.ParseSolution( solutionFileName )
+                                       .Projects
+                                       .Where( p => !(p is SolutionFolder) && p.Name != "CodeCakeBuilder" );
+
+            // We publish .Tests projects for this solution.
+            var projectsToPublish = projects;
+
+            SimpleRepositoryInfo gitInfo = Cake.GetSimpleRepositoryInfo();
+            StandardGlobalInfo globalInfo = CreateStandardGlobalInfo( gitInfo )
+                                                .AddNuGet( projectsToPublish )
+                                                .SetCIBuildTag();
+
+            Task( "Check-Repository" )
+                .Does( () =>
+                {
+                    globalInfo.TerminateIfShouldStop();
+                } );
+
+            Task( "Clean" )
+                .IsDependentOn( "Check-Repository" )
+                .Does( () =>
+                 {
+                     Cake.CleanDirectories( projects.Select( p => p.Path.GetDirectory().Combine( "bin" ) ) );
+                     Cake.CleanDirectories( projects.Select( p => p.Path.GetDirectory().Combine( "obj" ) ) );
+                     Cake.CleanDirectories( globalInfo.ReleasesFolder );
+                     Cake.DeleteFiles( "Tests/**/TestResult*.xml" );
+                 } );
+
+            Task( "Build" )
+                .IsDependentOn( "Check-Repository" )
+                .IsDependentOn( "Clean" )
+                .Does( () =>
+                {
+                    StandardSolutionBuild( globalInfo, solutionFileName );
+                } );
+
+            Task( "Unit-Testing" )
+                .IsDependentOn( "Build" )
+                .WithCriteria( () => Cake.InteractiveMode() == InteractiveMode.NoInteraction
+                                     || Cake.ReadInteractiveOption( "RunUnitTests", "Run Unit Tests?", 'Y', 'N' ) == 'Y' )
+               .Does( () =>
+               {
+                    var testProjects = projects.Where( p => p.Name.EndsWith( ".Tests" ) );
+                    StandardUnitTests( globalInfo, testProjects );
+               } );
+
+            Task( "Create-NuGet-Packages" )
+                .WithCriteria( () => gitInfo.IsValid )
+                .IsDependentOn( "Unit-Testing" )
+                .Does( () =>
+                {
+                    StandardCreateNuGetPackages( globalInfo );
+                } );
+
+            Task( "Push-Artifacts" )
+                .IsDependentOn( "Create-NuGet-Packages" )
+                .WithCriteria( () => gitInfo.IsValid )
+                .Does( () =>
+                {
+                    globalInfo.PushArtifacts();
+                } );
+
+            // The Default task for this script can be set here.
+            Task( "Default" )
+                .IsDependentOn( "Push-Artifacts" );
+        }
+    }
+}
